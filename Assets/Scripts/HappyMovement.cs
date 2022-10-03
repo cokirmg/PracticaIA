8e2e521d0dbdbc7d49bb606d818177ac2a704c34
cokirmg <rodrigomg666@gmail.com>
2022-10-03 21:52:52 +0200

    Cambio 2

--- Assets/Scripts/HappyMovement.cs
+++ Assets/Scripts/HappyMovement.cs
@@ -5,13 +5,20 @@ using UnityEngine.AI;
 
 public class HappyMovement : MonoBehaviour
 {
+    public Vector3 values;
+    //public Transform[] barajas;
     public Transform barajas;
+    public int num = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+       // barajas = Transform.
+        //values[num] = barajas.Transform.position;
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = barajas.position;
+
+       // agent.destination = new Vector3[barajas.position[num]];
     }
 
     // Update is called once per frame
