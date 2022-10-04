b5264053c3126bc9f81f0e8d3246d0bd206fb6bb
cokirmg <rodrigomg666@gmail.com>
2022-10-04 09:12:07 +0200

    Movimiento, Areas y Waypoints

--- Assets/Scripts/HappyMovement.cs
+++ Assets/Scripts/HappyMovement.cs
@@ -5,25 +5,55 @@ using UnityEngine.AI;
 
 public class HappyMovement : MonoBehaviour
 {
-    public Vector3 values;
+    
     //public Transform[] barajas;
-    public Transform barajas;
-    public int num = 0;
-
+    public Transform[] barajas;
+    public int numPoint = 0;
+    public float velocity = 5f;
+    public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-       // barajas = Transform.
-        //values[num] = barajas.Transform.position;
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = barajas.position;
+       
+        agent = GetComponent<NavMeshAgent>();
+        //agent.destination = barajas.position;
+        agent.destination = barajas[numPoint].position;
 
-       // agent.destination = new Vector3[barajas.position[num]];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, barajas[numPoint].position) < 1f)
+        {
+            if(numPoint >= 6)
+            {
+                numPoint = 0;
+            }
+            else
+            {
+                numPoint++;
+                
+            }
+            agent.destination = barajas[numPoint].position;
+        }
+        else
+        {
+            agent.destination = barajas[numPoint].position;
+            //GetComponent<Rigidbody>().velocity = (barajas[numPoint].position - transform.position).normalized * velocity;
+        }
+
+        // Find the nearest point on water.
+        int sandMask = 1 << NavMesh.GetAreaFromName("Sand");
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(transform.position, out hit, 2.0f, sandMask))
+        {
+            agent.speed = 1.5f;
+        }
+        else
+        {
+            agent.speed = 3.5f;
+        }
     }
 }
+
