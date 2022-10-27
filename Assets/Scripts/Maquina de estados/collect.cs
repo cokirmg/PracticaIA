b742a30b8819bb55f1d5900c8d43abcd4779f0da
cokirmg <rodrigomg666@gmail.com>
2022-10-27 17:26:28 +0200

    xingi tu xingi lali no me ayuda porque claro patri dame un beso y luego fijate

---
+++ Assets/Scripts/Maquina de estados/collect.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collect : StateMachineBehaviour
+{
+    public int conteo;
+    public float secCollect;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        conteo = animator.gameObject.GetComponent<scan>().conteo;
+        secCollect = secCollect + 1 * Time.deltaTime;
+        if (secCollect >= 3)
+        {
+            if(conteo >= 3)
+                {
+
+            }
+            secCollect = 0;
+            
+
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Assets/Scripts/Maquina de estados/scan.cs
+++ Assets/Scripts/Maquina de estados/scan.cs
@@ -7,6 +7,8 @@ public class scan : StateMachineBehaviour
 {
     private NavMeshAgent agent;
     public float secScan;
+    public int conteo;
+    public Transform objetoRaycast;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -20,38 +22,56 @@ public class scan : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         agent.speed = 0f;
-        
+        Ray ray = new Ray(agent.transform.position, agent.transform.forward);
+        Debug.DrawRay(agent.transform.position, agent.transform.forward, Color.red);
+        RaycastHit toca;
+
         secScan = secScan + 1 * Time.deltaTime;
         if (secScan >= 5)
         {
+            if (Physics.Raycast(ray, out toca, 5f) && objetoRaycast != toca.transform)
+            {
+
+
+
+                if (toca.transform.tag == ("roca"))
+                {
+
+
+                    animator.SetBool("collect", true);
+                    conteo++;
+
+
+                }
+                else
+                {
+
+                    animator.SetBool("scan", false);
+                }
+
+                objetoRaycast = toca.transform;
+                //animator.SetBool("scan", false);
+                //hit.transform.tag = 
+            }
+            else
+            {
+                animator.SetBool("scan", false);
+            }
             secScan = 0;
             animator.SetBool("scan", false);
 
         }
 
 
-        Ray ray = new Ray(agent.transform.position, agent.transform.forward);
-        Debug.DrawRay(agent.transform.position, agent.transform.forward, Color.red);
-        RaycastHit toca;
-        if (Physics.Raycast(ray, out toca, 5f))
-        {
-            
-
-
-            if (toca.transform.tag == ("roca"))
-            {
-                
-            }
-
-            //animator.SetBool("scan", false);
-            //hit.transform.tag = 
-        }
+        
+        
+        
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        animator.SetBool("collect", false);
     }
 
    
