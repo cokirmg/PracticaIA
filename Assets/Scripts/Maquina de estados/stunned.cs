e134a3ff77826944aa6e3e2ab4e7655125f207f1
cokirmg <rodrigomg666@gmail.com>
2022-10-31 21:50:24 +0100

    Otro dia que no hago nada

--- Assets/Scripts/Maquina de estados/stunned.cs
+++ Assets/Scripts/Maquina de estados/stunned.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class stunned : StateMachineBehaviour
 {
     public float secStunned;
+    public NavMeshAgent agent;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.gameObject.GetComponent<NavMeshAgent>();
+        agent.speed = 0f;
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
